7bf8bf19bc0e6808245d585ab388ceaf289f1f51
Hanazono Archive <jaymark23x@gmail.com>
2025-01-24 22:12:23 +0800

    New UI

---
+++ script/index.js
@@ -0,0 +1,11 @@
+document.getElementById('schedule_and_appointment').addEventListener('click', function() {
+    console.log("Working!")
+});
+
+document.getElementById('billing_and_invoice').addEventListener('click', function() {
+    console.log("Working!")
+});
+
+document.getElementById('tracking_and_report').addEventListener('click', function() {
+    console.log("Working!")
+});
