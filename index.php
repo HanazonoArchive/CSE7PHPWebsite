b62b90dcfeb740ad88077e30384e1b6071b5c17f
Hanazono Archive <jaymark23x@gmail.com>
2025-02-06 19:53:15 +0800

    Update 14

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ define('PROJECT_ROOT', $_SERVER['DOCUMENT_ROOT'] . '/CSE7PHPWebsite/public');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Field Service</title>
+    <title>Coolant</title>
 </head>
 
 <body>
